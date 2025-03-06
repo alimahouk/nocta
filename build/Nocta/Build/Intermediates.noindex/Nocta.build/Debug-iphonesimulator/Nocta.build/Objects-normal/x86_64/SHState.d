f3fa972de13abb73645669704f1f61d0acddd800
Ali Mahouk <ali.mahouk@icloud.com>
2025-03-06 22:58:10 +0400

    Remove build directory

--- build/Nocta/Build/Intermediates.noindex/Nocta.build/Debug-iphonesimulator/Nocta.build/Objects-normal/x86_64/SHState.d
+++
@@ -1,5 +0,0 @@
-dependencies: /Users/ali/Code/Archives/Nocta/Nocta/Classes/SHState.m \
-  /Users/ali/Code/Archives/Nocta/Nocta/Classes/SHState.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.3.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.3.sdk/usr/include/mach-o/dyld.modulemap \
-  /Users/ali/Code/Archives/Nocta/Nocta/Constants.h
